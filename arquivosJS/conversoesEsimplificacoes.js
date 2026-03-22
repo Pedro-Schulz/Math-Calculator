47cd8c673ea5fb8fc13fa339106bbc8ecb24218d
Pedro-Schulz <schulzpedroemanuel@gmail.com>
2026-03-22 17:35:11 -0300

    button adjustments

--- arquivosJS/conversoesEsimplificacoes.js
+++ arquivosJS/conversoesEsimplificacoes.js
@@ -1,7 +1,7 @@
 export function conversao() {
     let unidade1 = prompt("\n-> Qual a unidade a ser convertida? "); // Unidade de medida do valor inicial
     let unidade2 = prompt("\n-> Qual a unidade após a conversão? "); // Unidade de medida do valor convertido
-    let valor = parseFloat(prompt("\n-> Digite quantos", unidade1, "serão convertidos(as): ")); // Valor a ser convertido
+    let valor = parseFloat(prompt("\n-> Digite quantos" + unidade1 + "serão convertidos(as): ")); // Valor a ser convertido
     let valorConversao = parseInt(prompt("\n-> Digite o valor para realizar a conversão (ex: de km para metros = * 1000): ")); // Valor para realizar a conversão
     let opcao = parseInt(prompt("\n-> Digite 1 para conversões com multiplicação ou 2 para conversões com divisão")); // Escolha entre conversão com multiplicação ou divisão
     if (opcao == 1) { // Caso for multiplicação 
@@ -14,7 +14,7 @@ export function conversao() {
         alert("Erro! Opção de conversão inváida!");
         return
     }
-    alert("\nO resultado é: " + valor, unidade1 + " --> " + resultado, unidade2); // Output final
+    alert("\nO resultado é: " + valor + unidade1 + " --> " + resultado + unidade2); // Output final
 }
 export function simplificarFracao() {
     let numerador = parseInt(prompt("\n-> Digite o numerador da fração: "));
