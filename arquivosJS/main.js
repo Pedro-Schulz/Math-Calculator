fa816d0496efeb35ee02608676370415305f4f55
Pedro-Schulz <schulzpedroemanuel@gmail.com>
2026-03-22 17:19:15 -0300

    addEventListener and ID

--- arquivosJS/main.js
+++ arquivosJS/main.js
@@ -5,85 +5,21 @@ import { porcentagem, desconto, acrescimo } from "./percentuais.js";
 import { regra3, regra3composta } from "./regra3.js";
 import { mediaAritmetica, mediana, moda } from "./medias.js";
 
-let numero; // Variável
-
-// Interface e input para escolha de operação
-numero = parseInt(prompt(
-    `Escolha uma operação:
-    \n1. Soma
-    \n2. Subtração
-    \n3. Multiplicação
-    \n4. Divisão
-    \n5. Potência (x²)
-    \n6. Raiz Quadrada (√)
-    \n7. Simplificação de Frações
-    \n8. Conversão de valores
-    \n9. Porcentagem (%)
-    \n10. Acréscimo
-    \n11. Desconto
-    \n12. Equação de 1º grau
-    \n13. Equação de 2º grau
-    \n14. Regra de 3
-    \n15. Regra de 3 composta
-    \n16. Média Aritmética
-    \n17. Mediana
-    \n18. Moda\n`
-));
-
-// Estrutura condicional
-switch (numero) {
-case 1:
-    soma();
-    break
-case 2:
-    subtracao();
-    break
-case 3:
-    multiplicacao();
-    break
-case 4:
-    divisao();
-    break
-case 5:
-    potencia();
-    break
-case 6:
-    raizQuadrada();
-    break
-case 7:
-    simplificarFracao();
-    break
-case 8:
-    conversao();
-    break
-case 9:
-    porcentagem();
-    break
-case 10:
-    acrescimo();
-    break
-case 11:
-    desconto();
-    break
-case 12:
-    equacaoPrimeiroGrau();
-    break
-case 13:
-    equacaoSegundoGrau();
-    break
-case 14:
-    regra3();
-    break
-case 15:
-    regra3composta();
-    break
-case 16:
-    mediaAritmetica();
-    break
-case 17:
-    mediana();
-    break
-case 18:
-    moda();
-    break
-}
+document.getElementById("btnSoma").addEventListener("click", soma);
+document.getElementById("btnSubtracao").addEventListener("click", subtracao);
+document.getElementById("btnMultiplicacao").addEventListener("click", multiplicacao);
+document.getElementById("btnDivisao").addEventListener("click", divisao);
+document.getElementById("btnPotencia").addEventListener("click", potencia);
+document.getElementById("btnRaiz").addEventListener("click", raizQuadrada);
+document.getElementById("btnFracao").addEventListener("click", simplificarFracao);
+document.getElementById("btnConversao").addEventListener("click", conversao);
+document.getElementById("btnEquacao1").addEventListener("click", equacaoPrimeiroGrau);
+document.getElementById("btnEquacao2").addEventListener("click", equacaoSegundoGrau);
+document.getElementById("btnPorcentagem").addEventListener("click", porcentagem);
+document.getElementById("btnAcrescimo").addEventListener("click", acrescimo);
+document.getElementById("btnDesconto").addEventListener("click", desconto);
+document.getElementById("btnRegra3").addEventListener("click", regra3);
+document.getElementById("btnRegra3Composta").addEventListener("click", regra3composta);
+document.getElementById("btnMedia").addEventListener("click", mediaAritmetica);
+document.getElementById("btnMediana").addEventListener("click", mediana);
+document.getElementById("btnModa").addEventListener("click", moda);
